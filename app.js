28753b5eaa04e95f2cb3f96bcb7b9ee5f27d8005
inceptiongt <gaotong2991@gmail.com>
2017-09-26 18:37:17 +0800

    up

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const app = require('express')()
+const oauth = require('wechat-oauth-middleware')
 const config = require('./config')
 
-app.get('/wechat', async (req, res) =>{
-  const {echostr} = req.query
-  console.log(req.query)
-  const url = encodeURI('http://f81386c9.ngrok.io/auth')
-  const redirect = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${config.appid}&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=test#wechat_redirect `
-  res.redirect(redirect)
-})
-app.get('/auth', (req, res) => {
-  const {code} = req.query
-  const redirect = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${config.appid}&secret=${config.appsecret}&code=${code}&grant_type=authorization_code`
-  res.redirect(redirect)
+const oauthMiddleware = oauth({
+  appId: config.appid,
+  appSecret: config.appsecret,
+  scope: 'snsapi_userinfo',
+  host: 'http://192.168.1.165:8080' // 填入微信公众号回调域名
 })
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!');
+// app.get('/', (req, res) => {
+//   const {echostr} = req.query
+//   res.send(echostr)
+// })
+
+// app.get('/wechat/*', oauthMiddleware)
+
+app.get('/wechat/login', oauthMiddleware, function (req, res) {
+  console.log(req.wxUser || req.session.wxUser)
+})
+
+// app.get('/auth', (req, res) => {
+//   const {code} = req.query
+//   const redirect = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${config.appid}&secret=${config.appsecret}&code=${code}&grant_type=authorization_code`
+//   res.redirect(redirect)
+// })
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
